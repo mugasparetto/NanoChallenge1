5d90970053e64ef6f908b51090db7540e7269416
Murilo Gasparetto <murilogasparetto@MacBook-Pro-de-Murilo.local>
2015-03-20 14:07:02 -0300

    Initial Commit

---
+++ TesteImagemBotoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TesteImagemBotoes
+//
+//  Created by Murilo Gasparetto on 20/03/15.
+//  Copyright (c) 2015 Murilo Gasparetto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
