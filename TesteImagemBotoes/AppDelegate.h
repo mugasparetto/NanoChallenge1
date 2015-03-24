b10e2c6f72371fd58f94d1f350575648ab1873da
mugasparetto <mu.gasparetto@hotmail.com>
2015-03-24 18:14:17 -0300

    Retirei o ponto

--- TesteImagemBotoes/AppDelegate.h
+++ TesteImagemBotoes/AppDelegate.h
@@ -3,7 +3,7 @@
 //  TesteImagemBotoes
 //
 //  Created by Murilo Gasparetto on 20/03/15.
-//  Copyright (c) 2015 Murilo Gasparetto. All rights reserved.
+//  Copyright (c) 2015 Murilo Gasparetto. All rights reserved
 //
 
 #import <UIKit/UIKit.h>
