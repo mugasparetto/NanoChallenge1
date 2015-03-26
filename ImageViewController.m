214bf7f1e0957ba7ff6f3e450868ffe4712699b9
mugasparetto <mu.gasparetto@hotmail.com>
2015-03-26 09:41:45 -0300

    Revert "alo"
    This reverts commit 9d42395f011bc71ec798d674d68d61856e7db1cc.

--- ImageViewController.m
+++ ImageViewController.m
@@ -56,11 +56,7 @@
 }
 
 - (IBAction)praBaixo:(id)sender {
-<<<<<<< Updated upstream
     if (self.mario.frame.origin.y < 240) {
-=======
-    if (self.mario.frame.origin.y < 280) {
->>>>>>> Stashed changes
     [self.mario setFrame:CGRectMake(self.mario.frame.origin.x, self.mario.frame.origin.y + 20, self.mario.frame.size.width, self.mario.frame.size.height)];
        [self.mario setBounds:CGRectMake(0, 0, self.startSize.width, self.startSize.height)];
         [self.objetoContadorBaixo maisUm];
